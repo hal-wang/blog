a0b144a9029fd53057736c063e9287fcb9d332eb
hal-wang <hi@hal.wang>
2022-08-19 08:47:41 +0800

    ci: update publish

---
+++ publish.sh
@@ -0,0 +1,10 @@
+set -e
+
+npm install
+npm run build
+cd public
+git config --global user.name 'hal-wang' 
+git config --global user.email 'hi@hal.wang'
+git init -b gh-pages
+git add -A
+git commit -m "publish"
